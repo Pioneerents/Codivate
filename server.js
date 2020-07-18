a946c15a30c37f3de1571d3d005abe933e02c5d6
Mohamed Sheikh <mohamed-sheikh1@outlook.com>
2020-07-18 19:36:18 +0100

    added user check

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const fs = require("fs");
 const fileLocation = `${__dirname}/codivate_local.json`;
 const userFile = fs.readFileSync(fileLocation, "utf8");
 const users = JSON.parse(userFile);
+const { exec } = require("child_process");
+var a = "";
 
 app.use(
   bodyParser.text({
@@ -13,33 +15,49 @@ app.use(
   })
 );
 
-app.post("/submit", (req, res) => {
-  try {
-    const body = JSON.parse(req.body);
-    const number = body.number;
-    const doesUserExist = userCheck(number);
-    if (!doesUserExist) {
-      console.log("user dont exist");
-      users.push(body);
-      fs.writeFileSync(fileLocation, JSON.stringify(users), "utf8");
-      res.sendStatus(200);
-    } else {
-      console.log("User already exists");
-      res.sendStatus(409);
+app.post("/submit", async (req, res) => {
+  if (req.body && req.body.length > 4) {
+    try {
+      const body = JSON.parse(req.body);
+      const number = body.number;
+      const doesUserExist = await userCheck(req.body);
+      if (!doesUserExist) {
+        console.log("user dont exist");
+        users.push(body);
+        fs.writeFileSync(fileLocation, JSON.stringify(users), "utf8");
+        res.sendStatus(200);
+      } else {
+        console.log("User already exists");
+        res.sendStatus(409);
+      }
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    console.log(error);
   }
 });
 
-function userCheck(number) {
-  try {
-    return users.some((element) => {
-      return element.number === number;
+function userCheck(payload) {
+  return new Promise((resolve, reject) => {
+    exec(`python somefile.py ${payload}`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(error);
+        reject(err);
+      }
+      if (stdout) {
+        if (stdout == "False") {
+          resolve(false);
+          return false;
+        } else {
+          resolve(true);
+          return true;
+        }
+      }
+      if (stderr) {
+        console.log(stderr);
+        reject(err);
+      }
     });
-  } catch (error) {
-    console.log(error);
-  }
+  });
 }
 
 module.exports.app = app;
