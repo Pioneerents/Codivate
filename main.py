386181908dc2a11e13f621cb4606cdc561dead34
Senna Semakula-Buuza <senna.semakula@outlook.com>
2020-07-18 19:45:37 +0100

    add args to script

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
 import json
+import argparse
 from pprint import pprint
 from db import Client
 
 TABLE_NAME = "codivate_users"
+
+parser = argparse.ArgumentParser(
+    description='Create DynamoDB connection and insert data.\n\nCan also query database.')
+parser.add_argument('--table',
+                    default=None,
+                    type=str,
+                    help='Table name')
+parser.add_argument('--name',
+                    default=None,
+                    type=str,
+                    help='Name of user. Primary key consists of User and Phone number')
+parser.add_argument('--number',
+                    default=None,
+                    type=str,
+                    help='Phone number of user')
+
+args = parser.parse_args()
+
+cmd_args = {
+    "name": args.name,
+    "number": args.number
+}
 
 def read_file(file_name):
     """Function that parses a JSON file and fetches new entries"""
@@ -15,6 +38,7 @@ def read_file(file_name):
     else:
         return data
 
+
 def setup_db_conn():
     """Initiate database connection"""
     try:
@@ -23,6 +47,18 @@ def setup_db_conn():
         print(e)
     else:
         return db_conn
+
+def validate_user(primary_key):
+    """Check if a user exists in database"""
+    db = setup_db_conn()
+    table = db.create_table(TABLE_NAME)
+
+    if db.get_item(table, primary_key) is False:
+        print("1")
+        return True
+    else:
+        print("0")
+        return False
 
 def onboard_users():
     """Add users to the database"""
@@ -34,24 +70,25 @@ def onboard_users():
     # Add the users
     try:
         for user in new_users:
-            key = {"name": user['name'], user['number']}
+            key = {"name": user['name'], "number": user['number']}
+            row = {
+                "name": user['name'], "number": user['number'],
+                "country": user['country'], "tip": user['tipId']
+            }
             name = user['name']
-            db.add_item(table, key)
-            print(f"Adding user: {name} to database")
+            if db.get_item(table, {"name": user['name'], "number": user['number']}) is False:
+                print(f"Adding user: {name} to database")
+                db.add_item(table, row)
     except Exception as e:
         print("Unable to onboard users")
         print(e)
 
+
 def main():
     """Single entry point for application"""
-    onboard_users()
-    # }
-    # db_conn.add_item(users_table, item)
-    # db_conn.get_item(users_table, {"name": "Joef", "number": "084238423"})
-    # Check existence of item
-    # item = {"user"}
-    # db_conn.get_item()
-
+    # onboard_users()
+    key = {"name": cmd_args['name'], "number": cmd_args['number']}
+    validate_user(key)
     # Wait until the table exists.
     # users_table.meta.client.get_waiter('table_exists').wait(TableName='codivate_users')
     # # Print out some data about the table.
