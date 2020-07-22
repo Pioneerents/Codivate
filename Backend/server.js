83bbb450d9f25321cc9809b449ac902b8de7e66a
Mohamed Sheikh <mohamed-sheikh1@outlook.com>
2020-07-22 01:39:41 +0100

    frontend changes, added category, fixed api

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,8 @@ const userFile = fs.readFileSync(fileLocation, "utf8");
 const users = JSON.parse(userFile);
 const { exec } = require("child_process");
 const cors = require("cors");
+const countries = require("../Frontend/countries.json");
+const tweets = require("./SoftwareTips.json");
 
 app.use(
   bodyParser.text({
@@ -29,7 +31,7 @@ app.post("/submit", async (req, res) => {
       const name = body.name;
       // const doesUserExist = await userCheck(name, number);
       console.log("user dont exist");
-      console.log("status is", doesUserExist)
+      console.log("status is", doesUserExist);
       users.push(body);
       fs.writeFileSync(fileLocation, JSON.stringify(users), "utf8");
       res.sendStatus(200);
@@ -39,29 +41,40 @@ app.post("/submit", async (req, res) => {
   }
 });
 
+app.get("/api/countries", function (req, res) {
+  res.send(countries);
+});
+
+app.get("/api/tweets", function (req, res) {
+  res.send(tweets);
+});
+
 function userCheck(name, number) {
   return new Promise((resolve, reject) => {
-    exec(`python main.py --name ${name} --number ${number}`, (error, stdout, stderr) => {
-      if (error) {
-        console.log(error);
-        reject(err);
-      }
-      console.log("test", stdout, "bang")
-      console.log("after")
-      if (stdout) {
-        if (stdout.includes("1")) {
-          resolve(false);
-          return false;
-        } else {
-          resolve(true);
-          return true;
+    exec(
+      `python main.py --name ${name} --number ${number}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(error);
+          reject(err);
+        }
+        console.log("test", stdout, "bang");
+        console.log("after");
+        if (stdout) {
+          if (stdout.includes("1")) {
+            resolve(false);
+            return false;
+          } else {
+            resolve(true);
+            return true;
+          }
+        }
+        if (stderr) {
+          console.log(stderr);
+          reject(err);
         }
       }
-      if (stderr) {
-        console.log(stderr);
-        reject(err);
-      }
-    });
+    );
   });
 }
 
