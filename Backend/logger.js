519a0427122c585557864c44f0695b5a34cb82d5
Mohamed Sheikh <mohamed-sheikh1@outlook.com>
2020-07-25 12:01:33 +0100

    added node logging, python clear file

---
+++ Backend/logger.js
@@ -0,0 +1,18 @@
+const log4js = require("log4js");
+log4js.configure({
+  appenders: {
+    Log: {
+      type: "file",
+      filename: `${__dirname}/../Logs/codivateNode.log`,
+      maxLogSize: 10485760,
+      backups: 3,
+      compress: true,
+    },
+  },
+  categories: {
+    default: { appenders: ["Log"], level: "debug" },
+  },
+});
+const logger = log4js.getLogger("Log");
+
+module.exports = logger;
