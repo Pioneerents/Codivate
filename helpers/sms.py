25dadbc258315d33bb4f5ebc7024dacf1468f111
Senna Semakula-Buuza <senna.semakula@outlook.com>
2020-08-09 18:29:48 +0100

    handle countries that do not support non alpha numeric numbers

--- helpers/sms.py
+++ helpers/sms.py
@@ -11,7 +11,7 @@ AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
 SENDER = os.environ['SENDER']
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
-
+excl_countries = ["India", "Pakistan", "United States", "Netherlands"]
 
 def send_message(sender, recipient, msg, title, name):
     retry = 0
@@ -27,6 +27,15 @@ def send_message(sender, recipient, msg, title, name):
         except TwilioRestException as e:
             logging.error(f"Unable to send SMS to {recipient}", e)
             if e.code == 21211:
+                break
+            elif e.code == 21612:
+                logging.info(f"Retrying using non alphanumeric id for {recipient}")
+                time.sleep(2)
+                client.messages.create(
+                    to=recipient,
+                    from_=SENDER,
+                    body=f"*{title.capitalize()} Tip*\n\nHey {name},\n\n{msg}\n\nEnjoy the content?\nSupport us on: \nhttps://www.buymeacoffee.com/Codivate")
+                logging.info(f"Sent text message to {recipient}!")
                 break
             retry += 1
             logging.info(f"Retrying text. Attempt #{retry}")
