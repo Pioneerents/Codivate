b362ae8c1d2e2181bb68c8b73657e5efe0befdd7
Senna Semakula-Buuza <senna.semakula@outlook.com>
2020-08-10 22:27:20 +0100

    remove retry twilio

--- helpers/sms.py
+++ helpers/sms.py
@@ -15,31 +15,23 @@ client = Client(ACCOUNT_SID, AUTH_TOKEN)
 excl_countries = ["United States", "Canada"]
 
 def send_message(sender, recipient, msg, title, name, country):
-    retry = 0
-    while retry <= 3:
-        try:
-            from_num = "Codivate" if country not in excl_countries else US_NUMBER
-            msg = msg.replace("\\n", "\n")
-            message = client.messages.create(
+    try:
+        from_num = "Codivate" if country not in excl_countries else US_NUMBER
+        msg = msg.replace("\\n", "\n")
+        message = client.messages.create(
+            to=recipient,
+            from_=from_num,
+            body=f"*{title.capitalize()} Tip*\n\nHey {name},\n\n{msg}\n\nEnjoy the content?\nSupport us on: \nhttps://www.buymeacoffee.com/Codivate")
+        logging.info(f"Sent text message to {recipient}!")
+    except TwilioRestException as e:
+        logging.error(f"Unable to send SMS to {recipient}", e)
+        if e.code == 21211:
+            print(f"{recipient} is invalid. Not sending")
+        elif e.code == 21612:
+            logging.info(f"Retrying using non alphanumeric id for {recipient}")
+            time.sleep(2)
+            client.messages.create(
                 to=recipient,
-                from_=from_num,
+                from_=SENDER,
                 body=f"*{title.capitalize()} Tip*\n\nHey {name},\n\n{msg}\n\nEnjoy the content?\nSupport us on: \nhttps://www.buymeacoffee.com/Codivate")
             logging.info(f"Sent text message to {recipient}!")
-            break
-        except TwilioRestException as e:
-            logging.error(f"Unable to send SMS to {recipient}", e)
-            if e.code == 21211:
-                break
-            elif e.code == 21612:
-                logging.info(f"Retrying using non alphanumeric id for {recipient}")
-                time.sleep(2)
-                client.messages.create(
-                    to=recipient,
-                    from_=SENDER,
-                    body=f"*{title.capitalize()} Tip*\n\nHey {name},\n\n{msg}\n\nEnjoy the content?\nSupport us on: \nhttps://www.buymeacoffee.com/Codivate")
-                logging.info(f"Sent text message to {recipient}!")
-                break
-            retry += 1
-            logging.info(f"Retrying text. Attempt #{retry}")
-            time.sleep(3)
-            continue
