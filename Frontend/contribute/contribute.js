ca1d2587e032ca38ae6c3757d33be312d7b3e458
Mohamed Sheikh <mohamed.sheikh@bbc.co.uk>
2020-08-12 00:09:31 +0100

    wip

--- Frontend/contribute/contribute.js
+++ Frontend/contribute/contribute.js
@@ -5,19 +5,33 @@ const contributionCategories = document.getElementById(
   "contributionCategories"
 );
 const submit = document.getElementById("submit");
+submit.disabled = true;
 const contributionText = document.getElementById("contributionText");
-function validation(element, number) {
-  if (element.value.length < number) {
+const invalid = document.getElementById("invalidMessage");
+function validation(element, min, max) {
+  if (element.value.length < min) {
+    submit.disabled = true;
     element.classList.remove("is-valid");
     element.classList.add("is-invalid");
+    let text = element.value.length === 1 ? "Character" : "Characters";
+    invalid.innerHTML = `${element.value.length} ${text}. Tip must be at least ${min} characters`;
+
+    element.classList.add("is-valid");
+  } else if (element.value.length > max) {
+    submit.disabled = true;
+    element.classList.remove("is-valid");
+    element.classList.add("is-invalid");
+    invalid.innerHTML = `${element.value.length} Characters. Tip must be no more than 260 characters`;
   } else {
     element.classList.remove("is-invalid");
     element.classList.add("is-valid");
+    invalid.innerHTML = "";
+    submit.disabled = false;
   }
 }
 
-contributionTip.addEventListener("change", () => {
-  validation(contributionTip, 10);
+contributionTip.addEventListener("input", () => {
+  validation(contributionTip, 10, 280);
 });
 
 form.addEventListener("submit", logSubmit);
